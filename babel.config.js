c6ece94612462ae01a3bcf994fa8176f2507c3be
devbucket <flomueller1981@gmail.com>
2021-03-02 20:54:17 +0100

    Simplified the babel config.

--- babel.config.js
+++ babel.config.js
@@ -4,54 +4,19 @@ module.exports = (api) => {
   return {
     compact: true,
     presets: [
-      ['@babel/preset-env', {
-        loose: true,
-        modules: false,
-        corejs: { version: 3 },
-        useBuiltIns: 'entry',
-        exclude: ['transform-typeof-symbol'],
-      }],
-      ['@babel/preset-react', {
-        development: process.env.NODE_ENV !== 'production',
-        useBuiltIns: true,
-      }],
+      '@babel/preset-env',
+      '@babel/preset-react',
     ],
     plugins: [
-      ['@babel/plugin-proposal-class-properties', {
-        loose: true,
-      }],
-      ['@babel/plugin-proposal-object-rest-spread', {
-        loose: true,
-      }],
-      ['@babel/plugin-transform-runtime', {
-        corejs: false,
-        helpers: true,
-        // eslint-disable-next-line global-require
-        version: require('@babel/runtime/package.json').version,
-        regenerator: true,
-        useESModules: process.env.NODE_ENV !== 'test',
-        absoluteRuntime: require.resolve('@babel/runtime/package.json'),
-      }],
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread',
       '@babel/plugin-proposal-optional-chaining',
       '@babel/plugin-proposal-nullish-coalescing-operator',
     ],
     env: {
-      test: {
-        presets: [
-          '@babel/preset-env',
-          '@babel/preset-react',
-        ],
-      },
       development: {
         plugins: [
           'react-refresh/babel',
-        ],
-      },
-      production: {
-        plugins: [
-          ['transform-react-remove-prop-types', {
-            removeImport: true,
-          }],
         ],
       },
     },
